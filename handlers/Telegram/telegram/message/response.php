325925e00119a9a1aad0ceaace4ddb40138606ac
Greg Magarshak <greg@qbix.com>
2025-11-26 02:29:36 -0500

    Made better messages after intent and invite

--- handlers/Telegram/telegram/message/response.php
+++ handlers/Telegram/telegram/message/response.php
@@ -6,9 +6,14 @@ function Telegram_telegram_message_response($params)
 	$appId = $params['appId'];
 	$chatId = Telegram_Bot::chatIdForReply($params);
 	$text = Q_Text::get('Telegram/content');
-	if (Q::startsWith($params['update']['message']['text'], '/start ')
-	and !empty(Telegram::$startParam)) {
-		Telegram_Bot::sendMessage($appId, $chatId, $text['private']['authenticated']['BackToBrowser']);
+	list($resolvedAppId, $info) = Users::appInfo('telegram', $appId);
+	if (Q::startsWith($params['update']['message']['text'], '/start ')) {
+		$url = Q::ifset($info, 'url', Q_Config::expect('Q', 'web', 'appRootUrl'));
+		if (Q::startsWith(Telegram::$startParam, 'intent-')) {
+			Telegram_Bot::sendMessage($appId, $chatId, $text['private']['authenticated']['BackToBrowser'], compact('url'));
+		} else {
+			Telegram_Bot::sendMessage($appId, $chatId, $text['private']['invited']['Approved'], compact('url'));
+		}
 		return;
 	}
 }
