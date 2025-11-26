3b666a8019fc6b51fbbc8c4b6a022b91cf874495
Greg Magarshak <greg@qbix.com>
2025-11-26 02:39:49 -0500

    Translated some text

--- handlers/Telegram/telegram/message/response.php
+++ handlers/Telegram/telegram/message/response.php
@@ -8,10 +8,20 @@ function Telegram_telegram_message_response($params)
 	$text = Q_Text::get('Telegram/content');
 	list($resolvedAppId, $info) = Users::appInfo('telegram', $appId);
 	if (Q::startsWith($params['update']['message']['text'], '/start ')) {
-		$url = Q::ifset($info, 'url', Q_Config::expect('Q', 'web', 'appRootUrl'));
 		if (Q::startsWith(Telegram::$startParam, 'intent-')) {
+			$app = Q::app();
+			$url = Q_Uri::url(Q::ifset($info, 'url',
+				Q_Config::get('Users', 'uris', "$app/afterActivate",
+					Q_Config::expect('Q', 'web', 'appRootUrl')
+				)
+			));
 			Telegram_Bot::sendMessage($appId, $chatId, $text['private']['authenticated']['BackToBrowser'], compact('url'));
 		} else {
+			$url = Q_Uri::url(Q::ifset($info, 'url', 
+				Q::ifset($_SESSION, 'Streams', 'invite', 'appUrl',
+					Q_Config::expect('Q', 'web', 'appRootUrl')
+				)
+			));
 			Telegram_Bot::sendMessage($appId, $chatId, $text['private']['invited']['Approved'], compact('url'));
 		}
 		return;
